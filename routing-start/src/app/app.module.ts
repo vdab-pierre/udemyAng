cafc9818d13b1a759be6c633001a8d7862c45672
vdab-pierre @desktop <pierre.colin@vdab.be>
2018-01-26 16:59:07 +0100

    tm eerste hfdstk 116 navigation paths begin

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes } from "@angular/router";
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -11,6 +13,13 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+
+
+const appRoutes:Routes = [
+  {path:'',component:HomeComponent},
+  {path:'users',component:UsersComponent},
+  {path:'servers',component:ServersComponent}
+];
 
 @NgModule({
   declarations: [
@@ -25,7 +34,8 @@ import { ServersService } from './servers/servers.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
