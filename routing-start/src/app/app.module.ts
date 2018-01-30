01677b63a0879c99b7a2a9cfbd9eeb31d0b99155
vdab-pierre @desktop <pierre.colin@vdab.be>
2018-01-30 16:52:24 +0100

    tm 120

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -18,7 +18,8 @@ import { ServersService } from './servers/servers.service';
 const appRoutes:Routes = [
   {path:'',component:HomeComponent},
   {path:'users',component:UsersComponent},
-  {path:'servers',component:ServersComponent}
+  {path:'servers',component:ServersComponent},
+  {path:'user/:id',component:UserComponent}
 ];
 
 @NgModule({
